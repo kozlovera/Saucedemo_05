ecb7e3d525435222b0cb12cc86ae7bd5c3d86099
Vera Kozlova <kozlovera@tt.tt>
2022-11-22 21:51:56 -0800

    second commit

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -26,6 +26,10 @@ class TestSample:
     def test_login_skip(self):
         pass
 
+    @pytest.mark.skip(reason="test is skipped because, ")
+    def test_login_skip(self):
+        pass
+
     '''
     Example of xfail test
     '''
